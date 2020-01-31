83ff057fde33f2e80995fec8fbd33ddbe1bd0ad8
JoshBarrios <jbarrios.home@gmail.com>
2020-01-31 09:51:18 -0700

    fill

---
+++ RunModel.py
@@ -0,0 +1,59 @@
+'''
+Josh Barrios 1/17/2020
+Testing an LSTM model for text synthesis with a random string of text from Darwin's works
+'''
+
+from __future__ import print_function
+import numpy as np
+import sys
+import io
+
+with io.open('Darwin', encoding='utf-8') as f:
+    text = f.read().lower()
+print('corpus length:', len(text))
+
+chars = sorted(list(set(text)))
+print('total chars:', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+maxlen = 40
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def print_test_text(seed):
+    # Function invoked at end of each epoch. Prints generated text.
+
+    for diversity in [1.1, 1.4]:
+        print('----- diversity:', diversity)
+
+        generated = ''
+        sentence = seed
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+
+        for i in range(1200):
+            x_pred = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x_pred[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
+
+
+
